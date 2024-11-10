75e7f27234956fccf0b420f91e61fdd9c17b33a9
Percs <83934299+Percslol@users.noreply.github.com>
2024-11-10 00:02:46 -0600

    chore: update curltransport and scramjet

--- scramjet/index.js
+++
@@ -1,8 +0,0 @@
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const scramjetPath = resolve(__dirname, "dist");
-
-export { scramjetPath };
